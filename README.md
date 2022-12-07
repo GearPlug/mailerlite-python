174c6f55c40263f946ead2e1f6588590ed7de7f7
juanrios15 <juankrios15@gmail.com>
2022-12-07 17:01:58 -0500

    added:create subscribers, assign to group, and webhooks

--- README.md
+++ README.md
@@ -6,21 +6,56 @@ mailerlite-python is an API wrapper for MailerLite, written in Python
 ```
 pip install mailerlite-python
 ```
-
 ## Usage
 ```
 from mailerlite.client import Client
-
 client = Client('API_KEY')
 ```
 
 ### Subscribers
-
 #### List Subscribers
 ```
-subs = list_subscribers(params=None)
+subs = client.list_subscribers(params=None)
 # Optional params (dict):
 # filter[status] = Must be one of the possible statuses: active, unsubscribed, unconfirmed, bounced or junk.
 # limit = Defaults to 25
 # page = Defaults to 1
+```
+#### Create Subscriber
+```
+subscriber = {
+    "email": "juankrios15@gmail.com",
+    "fields": {
+        "name": "Carlos",
+        "last_name": "Burgos",
+        "city": "Bogotá",
+    }
+}
+sub = client.create_subscribers(subscriber)
+```
+#### List Groups
+```
+groups = client.list_groups(params=None)
+# Optional params (dict):
+# filter[name] = Returns partial matches
+# limit = An account can have at most a 250 groups
+# page = Defaults to 1
+# sort = Can be one of: name, total, open_rate, click_rate, created_at. Defaults to ascending order; prepend (-)
+```
+
+### Webhooks
+#### List all webhooks
+
+```
+webhooks = client.list_webhooks()
+```
+#### Create webhook
+
+```
+webhook = {
+    "name": "first webhook",
+    "events": ["subscriber.created"],
+    "url": "http://www.cartwright.info/eligendi-soluta-corporis-in-quod-ullam"
+}
+webhook_created = client.create_webhook(webhook)
 ```
--- mailerlite/client.py
+++ mailerlite/client.py
@@ -9,10 +9,10 @@ class Client(object):
         "Content-Type": "application/json",
         "Accept": "application/json"
     }
-    
+
     def __init__(self, api_key):
         self.headers.update(Authorization=f"Bearer {api_key}")
-    
+
     def list_subscribers(self, params=None):
         """
         Params:
@@ -21,11 +21,75 @@ class Client(object):
         page = Defaults to 1
         """
         return self.get('subscribers', params=params)
-    
+
+    def create_subscriber(self, data):
+        """
+        Request:
+            email: Valid email address (REQUIRED)
+            fields (object): Object keys must correspond to default or custom fields
+            groups (array): array must contain existing group ids.
+            status: Can be one of the following: active, unsubscribed, unconfirmed, bounced, junk
+            subscribed_at: Must be a valid date in the format yyyy-MM-dd HH:mm:ss
+            ip_address: Must be a valid ip address
+        """
+        return self.post('subscribers', data=data)
+
+    def unsubscribe_subscriber(self, subscriber_id):
+        """
+        Unsubscribe user subscribed.
+        """
+        return self.post(f'subscribers/{subscriber_id}/unsubscribe')
+
+    def assign_to_group(self, subscriber_id, group_id):
+        """
+        Assign specific user/subscriber to group
+        """
+        return self.post(f'subscribers/{subscriber_id}/groups/{group_id}')
+
+    def list_groups(self, params=None):
+        """
+        Params:
+        limit = An account can have at most a 250 groups
+        page = Defaults to 1
+        filter[name] = Returns partial matches
+        sort = Can be one of: name, total, open_rate, click_rate, created_at. Defaults to ascending order; prepend -, 
+               e.g. -total for descending order
+        """
+        return self.get('groups', params=params)
+
+    def list_webhooks(self):
+        """
+        List all webhooks
+        """
+        return self.get(f'webhooks')
+
+    def create_webhook(self, data):
+        """
+        Request
+        name: Not Required
+        events: (array) Must be one of the events in 'available events' list
+        url: connecting endpoint
+        """
+        return self.post('webhooks', data=data)
+
+    def delete_webhook(self, webhook_id):
+        """
+        Delete specific webhook using its ID
+        """
+        return self.delete(f'webhooks/{webhook_id}')
+
     def get(self, endpoint, params=None):
         response = self.request('GET', endpoint, params=params)
         return self.parse(response)
-    
+
+    def post(self, endpoint, params=None, data=None, headers=None, json=True):
+        response = self.request('POST', endpoint, params=params, data=data, headers=headers, json=json)
+        return self.parse(response)
+
+    def delete(self, endpoint, params=None):
+        response = self.request('DELETE', endpoint, params=params)
+        return self.parse(response)
+
     def request(self, method, endpoint, params=None, data=None, headers=None, json=True):
         _headers = self.headers
         if headers:
@@ -36,10 +100,10 @@ class Client(object):
         else:
             kwargs['data'] = data
         return requests.request(method, self.url + endpoint, params=params, headers=_headers, **kwargs)
-    
+
     def parse(self, response):
         status_code = response.status_code
-        if 'application/json' in response.headers['Content-Type']:
+        if 'Content-Type' in response.headers and 'application/json' in response.headers['Content-Type']:
             try:
                 r = response.json()
             except ValueError:
